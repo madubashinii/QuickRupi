f6c68bf925435a762a1e93cb5ef532e498110be2
madubashinii <nilakshimadubashini920@gmail.com>
2025-10-06 21:49:28 +0530

    Revert "Merge remote-tracking branch 'origin/main' into feature/admin-finance-nilakshi"
    This reverts commit 75d725a0ce8ff208f993377426ddad90b16b19aa, reversing
    changes made to 1efaca08bf73f5ba53f00c14a8004886dd146a28.

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,27 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import {
-    EXPO_PUBLIC_API_KEY,
-    EXPO_PUBLIC_AUTH_DOMAIN,
-    EXPO_PUBLIC_PROJECT_ID,
-    EXPO_PUBLIC_STORAGE_BUCKET,
-    EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    EXPO_PUBLIC_APP_ID
-} from "@env";
+
 
 const firebaseConfig = {
-    apiKey: EXPO_PUBLIC_API_KEY,
-    authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
-    projectId: EXPO_PUBLIC_PROJECT_ID,
-    storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    appId: EXPO_PUBLIC_APP_ID,
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase services for use throughout the application
-export const auth = getAuth(app);  // Authentication service
-export const db = getFirestore(app);  // Firestore database service
+export const auth = getAuth(app);
+export const db = getFirestore(app);
