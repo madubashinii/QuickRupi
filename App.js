27e1817f5e74057dfc9d87713dc9cf48e658d2dd
madubashinii <nilakshimadubashini920@gmail.com>
2025-10-08 18:54:53 +0530

    update App.js

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigator from './navigation/AppNavigator';
-import { NavigationContainer } from "@react-navigation/native";
+import LenderTabs from './navigation/LenderTabs';
+import NotificationsScreen from './screens/LenderScreens/Notifications';
+import MessagesScreen from './screens/LenderScreens/MessagesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="MainTabs" component={LenderTabs} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="Messages" component={MessagesScreen} />
+      </Stack.Navigator>
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
-
-
