ad0792c1efb7f3a858b3d4c1395c8737dbf699b4
madubashinii <nilakshimadubashini920@gmail.com>
2025-10-11 21:05:36 +0530

    add missing import in App.js

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import BorrowerDashboard from './screens/borrower/BorrowerDashboard';
 import AppNavigator from "./navigation/AppNavigator";
-import BorrowerBottomNav from "./navigation/borrower/borrowerNav";
-import LenderTabs from './navigation/LenderTabs';
-import NotificationsScreen from './screens/LenderScreens/Notifications';
-import MessagesScreen from './screens/LenderScreens/MessagesScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Toast from 'react-native-toast-message';
+
 const Stack = createNativeStackNavigator();
 
 
